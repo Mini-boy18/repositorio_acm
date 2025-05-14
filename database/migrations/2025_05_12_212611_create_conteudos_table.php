58461eba9f998a3a5df72289616371c0b2378f01
wxnchokg <77914426+HadesTV@users.noreply.github.com>
2025-05-14 17:38:13 +0100

    Novo

---
+++ database/migrations/2025_05_12_212611_create_conteudos_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateConteudosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('conteudos', function (Blueprint $table) {
+    $table->id();
+    $table->string('titulo');
+    $table->string('autor')->nullable();
+    $table->text('descricao')->nullable();
+    $table->text('arquivo_url');
+    $table->foreignId('area_id')->nullable()->constrained('areas')->nullOnDelete();
+    $table->foreignId('criado_por')->constrained('usuarios')->onDelete('cascade');
+    $table->timestamps();
+});
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('conteudos');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,17 @@
 <?php
-
+use App\Http\Controllers\UsuarioController;
+use App\Http\Controllers\AreaController;
+use App\Http\Controllers\ConteudoController;
+use App\Http\Controllers\AvaliacaoController;
+use App\Http\Controllers\RelatorioController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::resource('usuarios', UsuarioController::class);
+Route::resource('areas', AreaController::class);
+Route::resource('conteudos', ConteudosController::class);
+Route::resource('avaliacoes', AvaliacoesController::class);
 
+Route::get('/relatorios/acessos', [RelatorioController::class, 'index'])->name('relatorios.index');
 Route::get('/', function () {
     return view('home');
 });
